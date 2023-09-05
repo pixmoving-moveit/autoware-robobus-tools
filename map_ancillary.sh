686fdfe473ea15bdb1f4ccc3357ae97b5b4a342c
zymouse <zymouse@pixmoving.net>
2023-09-05 14:43:29 +0800

    fix:

--- map_ancillary.sh
+++ map_ancillary.sh
@@ -5,5 +5,5 @@ source $SCRIPT_DIR/../install/setup.bash
 ros2 launch  multi_lidar_calibration multi_lidar_calibration_ndt_map.launch.xml \
     pcd_path:=$SCRIPT_DIR/load_pcd/scans_down.pcd \
     input/target_pointcloud:=/sensing/lidar/ancillary/ouster/points \
-    initial_pose:='[1.35749, 1.92566, 0.0422595, 0.0862091, -0.00154772, 0.0241381 ]' \
+    initial_pose:='[-9.0, 1.92566, 0.0422595, 0.0862091, -0.00154772, -0.1 ]' \
     node_name:='ancillary_node'
--- map_ubfilter.sh
+++ map_ubfilter.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+source $SCRIPT_DIR/../install/setup.bash 
 
 ros2 launch  multi_lidar_calibration multi_lidar_calibration_ndt_map.launch.xml \
     pcd_path:=$SCRIPT_DIR/load_pcd/scans_down.pcd \
     input/target_pointcloud:=/sensing/lidar/concatenated/pointcloud_unfilter \
-    initial_pose:='[3.90265, 3.68133, 1.08971, 0.141197, 3.13604, 3.12115 ]' \
+    initial_pose:='[-5.23789, 0.523736, 0.997722, 0.0, 0.0, 3.3]' \
     node_name:='ubfilter_node'
