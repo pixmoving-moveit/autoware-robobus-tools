8ec00cdc4eeb88eae82821c7d958370a26d21467
zymouse <zymouse@pixmoving.net>
2023-12-28 09:54:29 +0800

    fix:多lidar测试

--- save_calibration_file.sh
+++ save_calibration_file.sh
@@ -19,7 +19,7 @@ child_frame='[]'
 output_yaml_file='sensors_calibration.yaml'
 
 cat<<EOF
-a: robobus sensor_kit_base_link 到 os_lidar_fl os_lidar_fr os_lidar_rl os_lidar_rr
+a: robobus sensor_kit_base_link 到 lidar_fl_base_link lidar_fr_base_link lidar_rl_base_link lidar_rr_base_link
 b: robobus sensor_kit_base_link 到 camera_top_link
 c: robobus sensor_kit_base_link 到 camera_front
 EOF
@@ -28,7 +28,7 @@ read -p "请输入标定序号(a-c): " choice
 case "$choice" in
     a)
         parent_frame='sensor_kit_base_link'
-        child_frame='[os_lidar_fl,os_lidar_fr,os_lidar_rl,os_lidar_rr]'
+        child_frame='[lidar_fl_base_link,lidar_fr_base_link,lidar_rl_base_link,lidar_rr_base_link]'
         output_yaml_file='sensor_kit_base_link2lidar-4.yaml'
     ;;
 
