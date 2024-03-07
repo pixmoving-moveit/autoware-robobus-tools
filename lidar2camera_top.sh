85bc9bcb3030320b963f0246a7923833777efc3a
zymouse <zymouse@pixmoving.net>
2024-03-07 10:19:33 +0800

    fix:lidar2camera

--- lidar2camera_top.sh
+++ lidar2camera_top.sh
@@ -3,4 +3,11 @@ SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 source $SCRIPT_DIR/../install/setup.bash 
 source $SCRIPT_DIR/../../autoware-robobus/install/setup.bash 
 
-ros2 launch robobus_sensor_kit_calibration tag_based.launch.xml
+if [[ "$1" == "true" ]]; then
+  ros2 launch robobus_sensor_kit_calibration interactive.launch.xml   # "参数值为True，执行操作。"
+else
+  ros2 launch robobus_sensor_kit_calibration tag_based.launch.xml
+fi
+
+
+
