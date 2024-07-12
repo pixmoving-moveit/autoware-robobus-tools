86b00d11140f4e3e3e1dc64037e15114dc176ddc
zymouse <zymouse@pixmoving.net>
2024-07-12 18:27:14 +0800

    fix:脚本

--- scripts/sensing.sh
+++ scripts/sensing.sh
@@ -7,15 +7,13 @@ function print_help() {
   echo "启动传感器"
   echo ""
   echo "Usage:"
-  echo "  ./sensing.sh [-robot_state] [-camera_type] [-verify]"
+  echo "  ./sensing.sh [-robot_state] [-verify]"
   echo ""
   echo "    - robot_state : 启动传感器是否发布tf树"
-  echo "    - camera_type : flir相机或者森云相机"
   echo "    - verify : 标定完成后验证tf"
   echo ""
 }
 
 robot_state="${1:-true}"
-camera_type="${2:-sensing}"
 
-ros2 launch robobus_sensor_kit_calibration sensing_all.launch.xml robot_state:=$robot_state camera_type:=$camera_type
+ros2 launch robobus_sensor_kit_calibration sensing_all.launch.xml robot_state:=$robot_state 
