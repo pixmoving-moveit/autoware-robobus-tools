cf5c1ae9cd203b51f617f1487c85a122aa3b75e2
zymouse <zymouse@pixmoving.net>
2024-07-17 15:25:51 +0800

    fix:优化to_pcd_png.sh, 生成的自动覆盖

--- scripts/to_pcd_png.sh
+++ scripts/to_pcd_png.sh
@@ -65,83 +65,141 @@ check_suffix_files() {
   fi
 }
 
-function process()
-{
-  path=$root/$1
+function rosbag_to_pcds(){
+  local rosbag_path=$1
+  local topic=$2
 
-  lidar_topic="/front/livox/points"
-  image_topics="['/sensing/camera/camera0/image_raw']"
-
-  check_topic_in_bag $lidar_topic $path
-  if [ $? -eq 0 ]; then
-    log_info "话题存在: $lidar_topic"
-    is_lidar_topic=ture
-  else
-    log_warning "话题不存在: $lidar_topic"
-    is_lidar_topic=flase
+  if [ -z "$rosbag_path" ]; then
+    log_error "rosbag_to_pcds()函数: rosbag_path变量是空的"
+    return
   fi
 
-  eval "imag_topics=(${image_topics//[\[\]\' ]/})"
-  # 遍历image_topics列表并调用check_topic_in_bag函数
-  for topic in "${imag_topics[@]}"; do
-    check_topic_in_bag $topic $path
-    if [ $? -eq 0 ]; then
-      log_info "话题存在: $lidar_topic"
-      is_image_topics=ture
-    else
-      log_warning "话题不存在: $lidar_topic"
-      is_image_topics=flase
-    fi
-  done
-  
+  check_topic_in_bag $topic $rosbag_path
+  if [ $? -eq 0 ]; then
+    log_info "话题存在: $topic"
+  else
+    log_warning "话题不存在: $topic"
+    return 
+  fi
 
-  ros2 launch $SCRIPT_DIR/launcher/rosbag2_to_pcd_png.launch.xml \
-      path:=$path \
-      lidar_topic:=$lidar_topic \
-      image_topics:=$image_topics \
-      is_image_topics:=$is_image_topics
-      is_lidar_topic:=$is_lidar_topic
+  ros2 launch $SCRIPT_DIR/launcher/rosbag2_to_pcds.launch.xml \
+    path:=$rosbag_path \
+    lidar_topic:=$topic 
 
-  mkdir $path"_pcd_png"
+  mkdir $rosbag_path"_pcd_png"
   
-  outpcds=$path"_pcds"
+  outpcds=$rosbag_path"_pcds"
   check_suffix_files $outpcds '.pcd'
   if [ $? -eq 0 ]; then
     log_info "合并pcd,输出output.pcd"
     cd $outpcds
     pcl_concatenate_points_pcd *.pcd
-    mv $path"_pcds/output.pcd" $path"_pcd_png"
+    mv $rosbag_path"_pcds/output.pcd" $rosbag_path"_pcd_png"
   fi
- 
+}
 
-  outpngs=$path"_pngs"
+function rosbag_to_pngs(){
+  log_info 
+  local rosbag_path=$1
+  local topics=$2
+
+  if [ -z "$rosbag_path" ]; then
+    log_error "rosbag_to_pngs()函数: rosbag_path变量是空的"
+    return
+  fi
+
+  eval "imag_topics=(${topics//[\[\]\' ]/})"
+  # 遍历image_topics列表并调用check_topic_in_bag函数
+  for topic in "${imag_topics[@]}"; do
+    check_topic_in_bag $topic $rosbag_path
+    if [ $? -eq 0 ]; then
+      log_info "话题存在: $topics"
+    else
+      log_warning "话题不存在: $topics"
+      return
+    fi
+  done
+
+  ros2 launch $SCRIPT_DIR/launcher/rosbag2_to_pngs.launch.xml \
+    path:=$rosbag_path \
+    image_topics:=$topics 
+  
+  mkdir $rosbag_path"_pcd_png"
+
+  outpngs=$rosbag_path"_pngs"
   check_suffix_files $outpngs '.png'
   if [ $? -eq 0 ]; then
     log_info "开始输出output.png"
     cd $outpngs
     last_file=$(ls -1 | tail -n 1)
     mv $last_file output.png
-    mv $path"_pngs/output.png" $path"_pcd_png"
+    mv $rosbag_path"_pngs/output.png" $rosbag_path"_pcd_png"
   fi
-  
+}
+
+function process()
+{
+  local path=$root/$1
+
+  local lidar_topic_1=$2
+  local image_topics_1=$3
+
+  log_info "start work: "$path 
+
+  rosbag_to_pcds $path $lidar_topic_1
+  rosbag_to_pngs $path $image_topics_1
 }
 
 function main()
 {
-  array=($(ls $root | grep rosbag2_2024))
+  lidar_topic="/sensing/lidar/concatenated/pointcloud_unfilter1111"
+  image_topics="['/sensing/camera/camera6/image_raw']"
+  
+  array=($(ls $root | grep rosbag2_))
   for element in "${array[@]}"
   do
     echo  $element
     log_info "start: $element"
-    process $element
+    process $element $lidar_topic $image_topics
     log_info "--------------------------------------------"
   done
 
-  rm -rf $root/output
-  mkdir $root/output
-  cd $root
-  mv *_pcd* $root/output
-  mv *_png* $root/output
+  find $root -type d -regextype egrep -regex '.*/ros2bag.*[0-9]+$' | while read dir_path; do
+    rosbag_name=$(basename "$dir_path")
+    pngs_name="$root/output/$rosbag_name"_pngs
+    pcds_name="$root/output/$rosbag_name"_pcds
+    png_pcd_name="$root/output/$rosbag_name"_pcd_png
+    
+    if [ -d "$pngs_name" ]; then
+      log_info "================================================"
+      mv "$root/$rosbag_name"_pngs/* $pngs_name
+      rm -r "$root/$rosbag_name"_pngs
+    else
+      log_warning "文件夹不存在: $pngs_name"
+      mkdir $root/output
+      mv "$root/$rosbag_name"_pngs/ $root/output
+    fi
+
+    if [ -d "$pcds_name" ]; then
+      log_info "================================================"
+      mv "$root/$rosbag_name"_pcds/* $pcds_name
+      rm -r "$root/$rosbag_name"_pcds 
+    else
+      log_warning "文件夹不存在: $pcds_name"
+      mkdir $root/output
+      mv "$root/$rosbag_name"_pcds/ $root/output
+    fi
+
+    if [ -d "$png_pcd_name" ]; then
+      log_info "================================================"
+      mv "$root/$rosbag_name"_pcd_png/* $png_pcd_name
+      rm -r "$root/$rosbag_name"_pcd_png
+    else
+      log_warning "文件夹不存在: $pcds_name"
+      mkdir $root/output
+      mv "$root/$rosbag_name"_pcd_png/ $root/output
+    fi
+  done
 }
 
 main 
