140c224560972dd5f6a3d249ffe454c58ffa68b4
zymouse <zymouse@pixmoving.net>
2024-07-24 15:40:59 +0800

    feat: 启动传感器

--- scripts/sensing.sh
+++ scripts/sensing.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
-source $SCRIPT_DIR/../install/setup.bash 
-source $SCRIPT_DIR/../../autoware-robobus/install/setup.bash 
+source $HOME/pix/robobus/autoware-robobus/install/setup.bash 
 
 function print_help() {
   echo "启动传感器"
   echo ""
   echo "Usage:"
-  echo "  ./sensing.sh [-robot_state] [-camera_type] [-verify]"
+  echo "  ./sensing.sh [-robot_state] [-verify]"
   echo ""
   echo "    - robot_state : 启动传感器是否发布tf树"
-  echo "    - camera_type : flir相机或者森云相机"
   echo "    - verify : 标定完成后验证tf"
   echo ""
 }
 
 robot_state="${1:-true}"
-camera_type="${2:-sensing}"
+autoware_robobus_calibration_path=$SCRIPT_DIR/..
 
-ros2 launch robobus_sensor_kit_calibration sensing_all.launch.xml robot_state:=$robot_state camera_type:=$camera_type
+ros2 launch $SCRIPT_DIR/launcher/sensor_driver_run.launch.xml robot_state:=$robot_state autoware_robobus_calibration_path:=$autoware_robobus_calibration_path
