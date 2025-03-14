27e34fa6359cfc6b1902782f3fe1c1fd9a86d589
zymouse <zymouse@pixmoving.net>
2025-03-14 18:05:08 +0800

    fix(scripts): 更新可以不修改内参文件和方便标定lidar2camera

--- scripts/launcher/camera/camera0.launch.py
+++ scripts/launcher/camera/camera0.launch.py
@@ -4,16 +4,19 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import yaml
 def generate_launch_description():
-    config_path = "/home/nvidia/pix/parameter/sensor_kit/robobus_sensor_kit_description/intrinsic_parameters/camera0.yaml"
+    camera_name="camera0"
+    config_path = f"{os.getenv('HOME')}/pix/parameter/sensor_kit/robobus_sensor_kit_description/intrinsic_parameters/{camera_name}.yaml"
     
     parameters={}
     with open(config_path, 'r') as yaml_file:
         parameters = yaml.safe_load(yaml_file)
+    parameters["frame_id"] = f"{parameters["frame_id"]}/cali"
+    
     node = Node(
             package='ros2_v4l2_jetcam',
             executable='ros2_v4l2_jetcam_node_exe',
-            name='ros2_v4l2_jetcam_node_exe',
-            namespace='camera0',
+            name=f"ros2_v4l2_jetcam_node_exe_{camera_name}",
+            namespace=camera_name,
             output='screen',
             parameters=[parameters],
             remappings=[
--- scripts/launcher/camera/camera2.launch.py
+++ scripts/launcher/camera/camera2.launch.py
@@ -1,36 +1,28 @@
-import argparse
 import os
-import sys
-import yaml
-
-
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
-
-
+from ament_index_python.packages import get_package_share_directory
+import yaml
 def generate_launch_description():
-    ld = LaunchDescription()
+    camera_name="camera2"
+    config_path = f"{os.getenv('HOME')}/pix/parameter/sensor_kit/robobus_sensor_kit_description/intrinsic_parameters/{camera_name}.yaml"
     
-    common_config_path = os.path.join(
-        os.getenv("HOME"), 
-        "pix/parameter/sensor_kit", 
-        "robobus_sensor_kit_description/intrinsic_parameters",
-        "camera2.yaml")
+    parameters={}
+    with open(config_path, 'r') as yaml_file:
+        parameters = yaml.safe_load(yaml_file)
+    parameters["frame_id"] = f"{parameters["frame_id"]}/cali"
     
-    video_encoder_path = os.path.join(
-        os.getenv("HOME"), 
-        "pix/parameter/sensor_kit", 
-        "robobus_sensor_kit_description/intrinsic_parameters",
-        "encoder_params.yaml")
-    
-    ld.add_action(Node(
-        package='isaac_ros_v4l2_camera', executable='isaac_ros_v4l2_camera_node_exe', output='screen',
-        name="usb_cam_camera2_node",
-        namespace='camera2',
-        parameters=[
-            {"common_config_path": common_config_path},
-            {"video_encoder_path": video_encoder_path},
+    node = Node(
+            package='ros2_v4l2_jetcam',
+            executable='ros2_v4l2_jetcam_node_exe',
+            name=f"ros2_v4l2_jetcam_node_exe_{camera_name}",
+            namespace=camera_name,
+            output='screen',
+            parameters=[parameters],
+            remappings=[
+                ('image_raw', 'camera_image'),
             ],
-        ))
-    return ld
+        )
+   
+    return LaunchDescription([node,
+                              ])
