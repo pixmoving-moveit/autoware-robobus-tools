c6b9d158e10a34c874a2f2997c9a56d35b8e3917
zymouse <zymouse@pixmoving.net>
2025-03-17 21:35:34 +0800

    fix(scripts): 实车测试

--- scripts/map_front.sh
+++ scripts/map_front.sh
@@ -5,6 +5,6 @@ source $SCRIPT_DIR/../install/setup.bash
 ros2 launch  multi_lidar_calibration multi_lidar_calibration_ndt_map.launch.xml \
     pcd_path:=$SCRIPT_DIR/load_pcd/scans_down.pcd \
     input/target_pointcloud:=/sensing/lidar/front/livox/points \
-    initial_pose:='[ -6.25645, -0.558192, 0.738937, 0.0, 0.0, 3.14]' \
+    initial_pose:='[ 0.0, -0.558192, 0.138937, 0.0, 0.0, 3.14]' \
     leaf_size:=0.6 \
     node_name:='livox_120'
