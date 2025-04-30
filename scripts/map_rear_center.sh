17476bf4cf3160c82e42c94459fbd89a48b9ad8c
zymouse <zymouse@pixmoving.net>
2025-04-30 13:29:35 +0800

    fix: 52th实车适配

--- scripts/map_rear_center.sh
+++ scripts/map_rear_center.sh
@@ -4,7 +4,7 @@ source $SCRIPT_DIR/../install/setup.bash
 
 ros2 launch  multi_lidar_calibration multi_lidar_calibration_ndt_map.launch.xml \
     pcd_path:=$SCRIPT_DIR/load_pcd/scans_down.pcd \
-    input/target_pointcloud:=/sensing/lidar/rear/center/points \
+    input/target_pointcloud:=/sensing/lidar/rear_center/points \
     initial_pose:='[4.1983,  1.3575, 0.73441,  0.0, 0.0, 0.0]'\
     leaf_size:=0.3 \
     node_name:='E1R_124'
--- scripts/map_rear_top.sh
+++ scripts/map_rear_top.sh
@@ -4,7 +4,7 @@ source $SCRIPT_DIR/../install/setup.bash
 
 ros2 launch  multi_lidar_calibration multi_lidar_calibration_ndt_map.launch.xml \
     pcd_path:=$SCRIPT_DIR/load_pcd/scans_down.pcd \
-    input/target_pointcloud:=/sensing/lidar/top/rear/points \
+    input/target_pointcloud:=/sensing/lidar/rear_top/points \
     initial_pose:='[4.1983,  1.3575, 1.73441,  0.0, 0.0, 0.0]'\
     leaf_size:=0.3 \
     node_name:='M1P_121'
