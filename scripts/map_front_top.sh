58b5ed493261dc3a6b65e2fc4d30ada33f71861e
zymouse <zymouse>
2025-05-21 23:54:31 +0800

    fix:找到lidar标定合适的标志物

--- scripts/map_front_top.sh
+++ scripts/map_front_top.sh
@@ -5,6 +5,6 @@ source $SCRIPT_DIR/../install/setup.bash
 ros2 launch  multi_lidar_calibration multi_lidar_calibration_ndt_map.launch.xml \
     pcd_path:=$SCRIPT_DIR/load_pcd/scans_down.pcd \
     input/target_pointcloud:=/sensing/lidar/front_top/points\
-    initial_pose:='[ 1.0, -0.558192, 1.138937, 0.0, 0.0, 3.2]' \
-    leaf_size:=0.2 \
+    initial_pose:='[-65.0, -7.0, 1.138937, 0.0, 0.0, -1.6]' \
+    leaf_size:=0.4 \
     node_name:='M1P_120'
