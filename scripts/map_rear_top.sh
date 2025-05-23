8a2e84b9e57edeab9023b7f072db7559d206b327
zymouse <zymouse@pixmoving.net>
2025-05-23 17:18:21 +0800

    fix:实车标定

--- scripts/map_rear_top.sh
+++ scripts/map_rear_top.sh
@@ -5,6 +5,6 @@ source $SCRIPT_DIR/../install/setup.bash
 ros2 launch  multi_lidar_calibration multi_lidar_calibration_ndt_map.launch.xml \
     pcd_path:=$SCRIPT_DIR/load_pcd/scans_down.pcd \
     input/target_pointcloud:=/sensing/lidar/rear_top/points \
-    initial_pose:='[-70.0, -30.0,  1.73441,  0.0, 0.0, -1.5]'\
+    initial_pose:='[-69.0, -30.0,  1.73441,  0.2, 0.0, -1.5]'\
     leaf_size:=0.5 \
     node_name:='M1P_121'
