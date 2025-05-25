5d7e5dbeb11ebedc2f4555cd518f5c11e5a76ea9
zymouse <zymouse@pixmoving.net>
2025-05-25 19:18:02 +0800

    fix

--- scripts/map_front_right.sh
+++ scripts/map_front_right.sh
@@ -5,7 +5,7 @@ source $SCRIPT_DIR/../install/setup.bash
 ros2 launch  multi_lidar_calibration multi_lidar_calibration_ndt_map.launch.xml \
     pcd_path:=$SCRIPT_DIR/load_pcd/scans_down.pcd \
     input/target_pointcloud:=/sensing/lidar/front_right/points \
-    initial_pose:='[0.5, 0.0, 0.0, 0.0, 3.14, -2.0]'\
+    initial_pose:='[0.5, -1.5, 0.0, 0.0, 3.14, -2.05]'\
     leaf_size:=0.1 \
     node_name:='Airy_123'
 
--- scripts/map_rear_top.sh
+++ scripts/map_rear_top.sh
@@ -5,6 +5,6 @@ source $SCRIPT_DIR/../install/setup.bash
 ros2 launch  multi_lidar_calibration multi_lidar_calibration_ndt_map.launch.xml \
     pcd_path:=$SCRIPT_DIR/load_pcd/scans_down.pcd \
     input/target_pointcloud:=/sensing/lidar/rear_top/points \
-    initial_pose:='[-0.5, 0.0, 0.0, 0.0, 0.0, 3.14]'\
+    initial_pose:='[-1.5, -0.5, 0.0, 0.0, 0.0, 3.14]'\
     leaf_size:=0.5 \
     node_name:='M1P_121'
