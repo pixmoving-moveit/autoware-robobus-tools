8f4a9a4e1bf77f156d584f54926c014b48038116
zymouse <zymouse@pixmoving.net>
2025-05-30 17:14:29 +0800

    fix(scipts): 多激光雷达标定一键全启

--- scripts/map_lidar_inft.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
-source $SCRIPT_DIR/../install/setup.bash 
-
-# ros2 run tf2_ros static_transform_publisher 0.5, 0.0, 0.0, 0.0, 0.0, -0.1 map lidar_ft_base_link
-
-ros2 run tf2_ros static_transform_publisher 0.5, 0.0, 0.0, -2.0, 3.14, -0.1 map lidar_fl_base_link
